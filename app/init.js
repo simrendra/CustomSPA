ac47e0f3d78c5436f6c8b254401fa5bcd7223ec5
pm-simrendrasingh-shahoo <simrendrasingh.shahoo@pubmatic.com>
2016-12-16 17:20:49 +0530

    added bootstrap

--- app/init.js
+++ app/init.js
@@ -6,13 +6,31 @@ requirejs.config({
         router: '/bower_components/crossroads/dist/crossroads',
         text: '/bower_components/text/text',
         sammy: '/bower_components/sammy/lib/min/sammy-latest.min',
-        jquery: '/bower_components/jquery/dist/jquery.min'
+        jquery: '/bower_components/jquery/dist/jquery.min',
+        bootstrap: '/bower_components/bootstrap/dist/js/bootstrap.min'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        }
     }
 
 });
 
 
-requirejs(['knockout', 'app', '/components/blog-menu/blog-menu.js', 'text', 'pages/home.js'], function (ko, app, blogMenu, text, home) {
+requirejs(['knockout', 'jquery', 'bootstrap', 'app', '/components/blog-menu/blog-menu.js', 'text', 'pages/home.js'], function (ko, jquery, bootstrap, app, blogMenu, text, home) {
+    
+    loadCss('bower_components/bootstrap/dist/css/bootstrap.min.css');
+    loadCss('bower_components/bootstrap/dist/css/bootstrap-theme.min.css');
 
-    // ko.applyBindings(app);
-})
+});
+
+
+
+function loadCss(url) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
